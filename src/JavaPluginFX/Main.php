d5d533f1b42d35ab6696060aedbfd630cf47fad5
EinfachNurNils <39995171+JavaPluginFX@users.noreply.github.com>
2018-06-22 22:40:12 +0200

    Create Main.php

---
+++ src/JavaPluginFX/Main.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace JavaPluginFX;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat as Color;
+use pocketmine\Player;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+
+class Main extends PluginBase implements Listener {
+	
+	public function onEnable() {
+		
+		$this->getServer()->getPluginManager ()->registerEvents ($this, $this);
+		$this->getServer()->getLogger()->info(Color::GRAY . "[" . Color::GREEN . "Gamemode" . Color::GRAY . "]" . Color::RED . "aktiviert");
+
+    }
+    
+		public function onCommand(CommandSender $sender, Command $cmd, string $label, array $args) : bool { 
+			
+			switch($cmd->getName() ) {
+				case "gms";
+				$sender->sendMessage (Color::GRAY . "[" . Color::GREEN . "Gamemode" . Color::GRAY . "]"  . Color::GREEN . " Dein Spielmodus wurde zu" . Color::RED . " Kreative" . Color::GREEN . "geändert");
+				$sender->setGamemode (0);
+				return true;
+				
+				case  "gmc";
+				$sender->sendMessage (Color::GRAY . "[" . Color::GREEN . "Gamemode" . Color::GRAY . "]" . Color::GREEN . " Dein Spielmodus wurde zu" . Color::RED . " Survival" . Color::GREEN . "geändert");
+				$sender->setGamemode (1);
+				return true;
+				
+				case  "gmt";
+				$sender->sendMessage (Color::GRAY . "[" . Color::GREEN . "Gamemode" . Color::GRAY . "]" . Color::GREEN . " Dein Spielmodus wurde zu" . Color::RED . " Spectator" . Color::GREEN . "geändert");
+				$sender->setGamemode (3);
+				return true;
+				
+    }
+    
+  }    
+  
+}    
